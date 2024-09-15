6b938cbbbdc4078e40e1bf3090742ed064e8b874
Friend A <101492420+Kousei-Friend-A@users.noreply.github.com>
2024-09-15 18:45:39 +0530

    Update text_utils.py

--- bot/core/text_utils.py
+++ bot/core/text_utils.py
@@ -191,7 +191,7 @@ class TextEditor:
     async def get_poster(self):
         if anime_id := await self.get_id():
             return f"https://img.anili.st/media/{anime_id}"
-        return "https://telegra.ph/file/112ec08e59e73b6189a20.jpg"
+        return "https://envs.sh/w1i.jpg"
 
     @handle_logs
     async def get_upname(self, qual=""):
