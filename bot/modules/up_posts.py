4df6e8854843acb7cabe40d3d39b1b41a595d7fd
Friend A <101492420+Kousei-Friend-A@users.noreply.github.com>
2024-09-16 12:36:18 +0530

    Update up_posts.py

--- bot/modules/up_posts.py
+++ bot/modules/up_posts.py
@@ -17,8 +17,9 @@ async def upcoming_animes():
             for i in aniContent:
                 aname = TextEditor(i["title"])
                 await aname.load_anilist()
-                text += f''' <a href="https://subsplease.org/shows/{i['page']}">{aname.adata.get('title', {}).get('english') or i['title']}</a>\n    • <b>Time</b> : {i["time"]} hrs\n\n'''
-            TD_SCHR = await bot.send_message(Var.MAIN_CHANNEL, text)
+                anime_list = f''' <a href="https://subsplease.org/shows/{i['page']}">{aname.adata.get('title', {}).get('english') or i['title']}</a>\n    • <b>Time</b> : {i["time"]} hrs\n\n'''
+                sch_list = text + anime_list + "<b>⏰ Current TimeZone :</b> <code>IST (UTC +5:30)</code>"
+            TD_SCHR = await bot.send_message(Var.MAIN_CHANNEL, sch_list)
             await (await TD_SCHR.pin()).delete()
         except Exception as err:
             await rep.report(str(err), "error")
